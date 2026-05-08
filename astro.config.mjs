e906ee22d318712a832b354a98542ae6732f40f2
Zhu Chengyi <123726248+zceeeeee@users.noreply.github.com>
2026-05-09 00:10:03 +0800

    Configure GitHub Pages root deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://zceeeeee.github.io',
-  base: '/personal_web'
+  site: 'https://zceeeeee.github.io'
 });
